7c69dac978000830fe5b0578e5b902c90c829420
JarvisK032 <jarvistyk@gmail.com>
2022-12-12 15:42:01 -0700

    updated kills

--- Shooting.Game/Scenes/Game/Casting/Score.cs
+++ Shooting.Game/Scenes/Game/Casting/Score.cs
@@ -7,12 +7,12 @@ namespace Example.Breaker.Game
     public class Score : Byui.Games.Casting.Label
     {
         private int _points = 0;
-        
+
         public Score() { }
 
         public override string GetText()
         {
-            return $"SCORE: {_points:000000000}";
+            return $"KILLS: {_points:0}";
         }
 
         public void AddPoints(int points)
